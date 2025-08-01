ac91cf7ded9c6d70ec30bcfbc41c098d835cd972
Andrescampi <pipeloon424@gmail.com>
2025-07-31 21:07:26 -0500

    Last changes of today

--- LogicaJavaScript/Dos/app.js
+++ LogicaJavaScript/Dos/app.js
@@ -1,9 +1,53 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML ="Juego del número secreto";
+// Esta variable es de alcance global
+let numeroSecreto = 0;
+let intentos = 0;
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Escoge un número del 1 al 10";
-
-function intentoDelUsuario(){
-    alert("El usuario ha intentado");
+function asignarTexto(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento(){
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if(numeroSecreto === numeroUsuario){
+        asignarTexto('p',`Felicitaciones, adivinaste el número secreto en ${intentos} ${(intentos === 1)? "intento" : "intentos"}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+    else{
+        if(numeroSecreto > numeroUsuario){
+        asignarTexto('p','El número secreto es mayor');
+        }
+        else{
+        asignarTexto('p','El número secreto es menor');
+        }
+        intentos++;
+        limpiarCaja();
+        console.log(intentos); 
+    }
+    return;
+}
+
+function nuevoJuego(){
+    limpiarCaja();
+    condicionesIniciales()
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function condicionesIniciales(){
+    asignarTexto('h1','Juego del número secreto');
+    asignarTexto('p','Escoge un número del 1 al 10');
+    intentos = 1;
+    numeroSecreto = generarNumeroAleatorio();
+}
+
+function limpiarCaja(){
+    document.getElementById('valorUsuario').value = '';
+}
+
+function generarNumeroAleatorio(){
+    // Esta variable es de alcance de bloque
+    return Math.floor(Math.random()*10)+1;;
+}
+
+condicionesIniciales();
