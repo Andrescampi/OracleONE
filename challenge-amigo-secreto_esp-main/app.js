448c6f59287eed753ccfd10bcf348ae8e2283459
Andrescampi <pipeloon424@gmail.com>
2025-08-22 18:48:02 -0500

    Add friend

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+let amigos = [];
+let nombre;
+
+function agregarNombres(){
+    nombre = document.getElementById("amigo").value;
+    if(nombre.trim() === ""){
+        alert("Debes introducir un valor");
+    }
+    else{
+        nombres.push(nombre);
+        limpiarCaja();        
+    }
+    limpiarCaja();
+    console.log(nombres);
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+}
+
+function mostrarNombre(){
+}
