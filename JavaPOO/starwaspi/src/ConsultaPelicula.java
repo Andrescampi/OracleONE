b3639b1360e1c25ad464d49d590c5243582963ff
Andrescampi <pipeloon424@gmail.com>
2026-02-05 10:15:31 -0500

    Add Star Wars API Java client and project files challenge
    Initial project scaffold and implementation to query SWAPI films and save results as JSON. Adds CLI entrypoint (Principal), HTTP client (ConsultaPelicula), Pelicula record model, and a JSON file generator (GeneradorDeArchivo). Also includes IntelliJ project files, .gitignore, and an .iml referencing gson-2.13.2 dependency.

---
+++ JavaPOO/starwaspi/src/ConsultaPelicula.java
@@ -0,0 +1,26 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+
+public class ConsultaPelicula {
+    public Pelicula buscaPelicula(int numeroPelicula){
+        URI direccion =  URI.create("https://swapi.dev/api/films/"+numeroPelicula);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.valueOf(direccion)))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontre esa pelicula.");
+        }
+    }
+}
