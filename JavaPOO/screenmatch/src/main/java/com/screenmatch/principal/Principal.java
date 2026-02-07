5fff2a9b3185251faf2919b63a0c50f63cc65c10
Andrescampi <pipeloon424@gmail.com>
2026-02-07 13:29:40 -0500

    End the project of screenmatch

--- JavaPOO/screenmatch/src/main/java/com/screenmatch/principal/Principal.java
+++ JavaPOO/screenmatch/src/main/java/com/screenmatch/principal/Principal.java
@@ -107,6 +107,13 @@ public class Principal {
                 .collect(Collectors.groupingBy(Episodio::getTemporada,
                         Collectors.averagingDouble(Episodio::getEvaluacion)));
         System.out.println(evaluacionesPorTemporada);
-//
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+        System.out.println("Media " + est.getAverage());
+        System.out.println("Mejor episódio: " + est.getMax());
+        System.out.println("Peor episódio: " + est.getMin());
+        System.out.println("Cantidad " + est.getCount());
     }
 }
